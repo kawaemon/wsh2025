4e143c84275289e08987243ef592fd6e0857b62e
kawaemon <me@kawaemon.dev>
2025-03-22 14:08:17 +0900

    remove reactscan

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -72,7 +72,6 @@ export function registerSsr(app: FastifyInstance): void {
         <head>
           <meta charSet="UTF-8" />
           <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-          <script src="https://unpkg.com/react-scan/dist/auto.global.js"></script>
           <script src="/public/main.js"></script>
           ${imagePaths.map((imagePath) => `<link as="image" href="${imagePath}" rel="preload" />`).join('\n')}
         </head>
