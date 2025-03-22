d7d8618907026b447b8db5f39777c7c0e0f67bcc
kawaemon <me@kawaemon.dev>
2025-03-22 19:24:04 +0900

    remove unused import

--- workspaces/client/src/pages/timetable/hooks/useSelectedProgramId.ts
+++ workspaces/client/src/pages/timetable/hooks/useSelectedProgramId.ts
@@ -1,7 +1,3 @@
-import { StandardSchemaV1 } from '@standard-schema/spec';
-import * as schema from '@wsh-2025/schema/src/api/schema';
-import { ArrayValues } from 'type-fest';
-
 import { useStore } from '@wsh-2025/client/src/app/StoreContext';
 
 export function useSelectedProgramId() {
