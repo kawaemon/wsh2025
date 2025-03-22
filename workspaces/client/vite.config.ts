73a80c519455958bec99f0b999d847c22c12f0c4
kawaemon <me@kawaemon.dev>
2025-03-23 05:30:10 +0900

    async

--- workspaces/client/vite.config.ts
+++ workspaces/client/vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/public',
   build: {
+    modulePreload: true,
     rollupOptions: {
       output: {
         chunkFileNames: 'chunk-[hash].js',
