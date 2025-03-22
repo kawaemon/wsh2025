eebefe69910a27bb3e91e0a3035be7abf6565604
kawaemon <me@kawaemon.dev>
2025-03-22 12:05:08 +0900

    nodelay on route

--- workspaces/client/src/app/Document.tsx
+++ workspaces/client/src/app/Document.tsx
@@ -4,7 +4,7 @@ import { Outlet, ScrollRestoration } from 'react-router';
 import { createStore } from '@wsh-2025/client/src/app/createStore';
 import { Layout } from '@wsh-2025/client/src/features/layout/components/Layout';
 
-export const prefetch = async (store: ReturnType<typeof createStore>) => {
+export const prefetchUser = async (store: ReturnType<typeof createStore>) => {
   const user = await store.getState().features.auth.fetchUser();
   return { user };
 };
