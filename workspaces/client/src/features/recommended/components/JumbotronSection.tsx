ef8d1063c53e2abc61dfa440f7de00f34ad79a9e
kawaemon <me@kawaemon.dev>
2025-03-23 03:35:43 +0900

    prevent layout shift on top

--- workspaces/client/src/features/recommended/components/JumbotronSection.tsx
+++ workspaces/client/src/features/recommended/components/JumbotronSection.tsx
@@ -40,7 +40,7 @@ export const JumbotronSection = ({ module }: Props) => {
             </div>
 
             <Flipped stagger flipId={isTransitioning ? `episode-${episode.id}` : 0}>
-              <div className="h-full w-auto shrink-0 grow-0">
+              <div className="aspect-16/9 h-full w-auto shrink-0 grow-0">
                 <Player
                   loop
                   className="size-full"
